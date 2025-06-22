a4f81cc404d0992c4ef53e252106a7d1e18597c2
Won <ayg1044@naver.com>
2025-06-22 18:06:01 +0900

    6/22

--- member/views.py
+++ member/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from .models import Member
 
 # Create your views here.
 def signup_page(request):
-    return render(request, 'signup.html')
+    if request.method == "GET":
+        return render(request, 'signup.html')
+    else:
+        id = request.POST.get('id')
+        pw = request.POST.get('pw')
+        name = request.POST.get('name')
+        nicname = request.POST.get('nicname')
+        print("id = ", id)
+        print("pw = " + pw)
+        print("name = " + name)
+        print("nicname = " + nicname)
+        return JsonResponse({"success":False, "error":"테스트 중입니다."})
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        # 로그인 검증 로직
+        print("로그인")
+        return redirect('/BapGo/start/')
