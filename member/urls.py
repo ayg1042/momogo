2cb5294ad7ff6a04679f137ec6139291dbdc385c
Won <ayg1044@naver.com>
2025-06-22 19:15:05 +0900

    로그인 회원가입입

--- member/urls.py
+++ member/urls.py
@@ -7,4 +7,5 @@ app_name = 'member'
 urlpatterns = [
     path('member/', views.login, name='login'),
     path('signup/', views.signup_page, name='signup'),
+    path('logout/', views.logout, name='logout'),
 ]
--- member/views.py
+++ member/views.py
@@ -11,16 +11,34 @@ def signup_page(request):
         pw = request.POST.get('pw')
         name = request.POST.get('name')
         nicname = request.POST.get('nicname')
-        print("id = ", id)
-        print("pw = " + pw)
-        print("name = " + name)
-        print("nicname = " + nicname)
-        return JsonResponse({"success":False, "error":"테스트 중입니다."})
+
+        if Member.objects.filter(id = id).exists():# select id from Member where id = id;
+            return JsonResponse({"success":False, "error":"이미 존재하는 아이디입니다."})
+        
+        Member.objects.create(id=id, pw=pw, name=name, nicname=nicname)
+
+        request.session['user_id'] = id
+
+        return JsonResponse({"success":True, "error":"회원가입 되었습니다."})
 
 def login(request):
     if request.method == "GET":
+        if request.session.get('user_id'):
+            return redirect('/BapGo/start/')
         return render(request, 'login.html')
     else:
         # 로그인 검증 로직
-        print("로그인")
-        return redirect('/BapGo/start/')
+        id = request.POST.get('userName')
+        pw = request.POST.get('userPassword')
+
+        user = Member.objects.filter(id=id, pw=pw).first()
+        if user:
+            request.session['user_id'] = id
+            return redirect('/BapGo/start/')    
+        else:
+            return render(request, 'login.html', {'error_message':"아이디 또는 비밀번호가 일치하지 않습니다."})
+        
+def logout(request):
+    if 'user_id' in request.session:
+        del request.session['user_id']
+    return redirect('/')
